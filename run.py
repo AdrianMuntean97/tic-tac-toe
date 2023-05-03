9fe5933f2a3d478e92e67fccf7bf7a202d5d6ffa
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2023-05-03 08:23:26 +0000

    Add get_player_move function where the user has to input a value for a row and a column, and check to see if the input is valid

--- run.py
+++ run.py
@@ -7,3 +7,22 @@ def print_board(board):
     """
     for row in board:
         print(" ".join(row))
+
+
+def get_player_move(board):
+    """
+    Get input from the player and print the value provided.
+    Check to see if the input provided has already been used,
+    and return invalid input if the user didn't provide and integer.
+    """
+    while True:
+        try:
+            row, col = map(int, input("Enter row and column(example: 1 2): ").split())
+            if board[row][col] == "_":
+                return row,col
+            else:
+                print("That cell is already occupied. Please try again")
+            except (ValueError, IndexError):
+                print("Invalid input. Please try again.")
+
+
